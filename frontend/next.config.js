ea8b9646e774e76575446945fe5b5b6f5ad6b2da
Jillian-ignited <Jillian@ignitedindustries.com>
2025-09-24 13:53:05 -0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: 'export',        // static export
+  trailingSlash: true,     // ensures folder-based paths (…/index.html)
 };
 module.exports = nextConfig;
